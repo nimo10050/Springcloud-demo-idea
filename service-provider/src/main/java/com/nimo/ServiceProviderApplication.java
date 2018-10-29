927ff6b55a461f6a6271c63646e8683320416afa
张高培 <m15639160073@163.com>
2018-10-29 14:49:01 +0800

    服务提供者

---
+++ service-provider/src/main/java/com/nimo/ServiceProviderApplication.java
@@ -0,0 +1,29 @@
+package com.nimo;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+public class ServiceProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProviderApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String sayHello(@RequestParam(value = "name", defaultValue = "zgp") String name) {
+        return "hi " + name + " ,I am from port:" + port;
+    }
+
+}
